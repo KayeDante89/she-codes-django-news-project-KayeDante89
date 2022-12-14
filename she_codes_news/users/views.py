1de4aa05afe35ba89b2c5c61e5fb3c2ea8b3d16f
KayeDante89 <k.flamenco@hotmail.com>
2022-12-14 23:18:03 +0800

    tidied my profile page

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -26,5 +26,5 @@ class Profile(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user_stories'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+        context['user_stories'] = NewsStory.objects.filter(author=self.kwargs['pk']).order_by('-pub_date')
         return context
